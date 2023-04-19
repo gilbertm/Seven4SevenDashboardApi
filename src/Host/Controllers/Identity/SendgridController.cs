6aa3e4027fa5c2da9891ae50ca4947c977187a01
GTM Seven4Seven <gilbert_maloloyon@outlook.com>
2023-04-19 11:17:45 +0400

    alignment with machines. jlt

---
+++ src/Host/Controllers/Identity/SendgridController.cs
@@ -0,0 +1,65 @@
+using Microsoft.Identity.Client;
+using RAFFLE.WebApi.Application.Identity.Users;
+using RAFFLE.WebApi.Application.SevenFourSeven.Bridge;
+using RAFFLE.WebApi.Application.SevenFourSeven.Raffle;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Twilio.Rest.Verify.V2.Service;
+using Twilio;
+using RAFFLE.WebApi.Application.SevenFourSeven.SendgridTwilio;
+
+namespace RAFFLE.WebApi.Host.Controllers.Identity;
+
+public class SendgridController : VersionNeutralApiController
+{
+
+    private readonly IConfiguration _config;
+
+    public SendgridController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    /// <summary>
+    /// Send an email verification code from Twilio (SendGrid).
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    [HttpPost("send-code")]
+    [TenantIdHeader]
+    [AllowAnonymous]
+    [OpenApiOperation("Send an email verification code", "")]
+    [ApiConventionMethod(typeof(RAFFLEApiConventions), nameof(RAFFLEApiConventions.Register))]
+    public VerificationResource SendCode([FromBody] string email)
+    {
+        TwilioClient.Init(_config.GetSection("SevenFourSevenAPIs:Twilio:AccountSID").Value!, _config.GetSection("SevenFourSevenAPIs:Twilio:AuthToken").Value!);
+
+        var verification = VerificationResource.Create(
+            to: email,
+            channel: "email",
+            pathServiceSid: $"{_config.GetSection("SevenFourSevenAPIs:Twilio:ServiceId").Value!}"
+            );
+
+        return verification;
+    }
+
+    /// <summary>
+    /// Verifies the email sent. Twilio (SendGrid).
+    /// </summary>
+    /// <param name="twilioCodeRequest"></param>
+    /// <returns>VerificationCheckResource.</returns>
+    [HttpPost("verify-code")]
+    [TenantIdHeader]
+    [AllowAnonymous]
+    [OpenApiOperation("Verify code", "")]
+    [ApiConventionMethod(typeof(RAFFLEApiConventions), nameof(RAFFLEApiConventions.Register))]
+    public VerificationCheckResource VerifyCodeAsync([FromBody] TwilioCodeRequest twilioCodeRequest)
+    {
+        TwilioClient.Init(_config.GetSection("SevenFourSevenAPIs:Twilio:AccountSID").Value!, _config.GetSection("SevenFourSevenAPIs:Twilio:AuthToken").Value!);
+
+        var verificationCheck = VerificationCheckResource.Create(to: twilioCodeRequest.Email, code: twilioCodeRequest.Code, pathServiceSid: $"{_config.GetSection("SevenFourSevenAPIs:Twilio:ServiceId").Value!}");
+
+        return verificationCheck;
+    }
+}
