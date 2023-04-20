f7e52cf473f00da2b359fda8feef61d9d917c1a3
Gilbert PC <gilbert_maloloyon@outlook.com>
2023-04-21 01:58:22 +0400

    player entries

--- src/Core/Application/SevenFourSeven/Raffle/GetEntriesRequest.cs
+++ src/Core/Application/SevenFourSeven/Raffle/GetEntriesRequest.cs
@@ -5,7 +5,7 @@ public class GetEntriesRequest
     public string AuthCode { get; set; } = default!;
     public string UserId747 { get; set; } = default!;
     public string UserName747 { get; set; } = default!;
-    public string RaffleId { get; set; } = default!;
-    public int Page { get; set; } = 0;
-    public int PageSize { get; set; } = 0;
+    public int RaffleId { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; } 
 }
--- src/Core/Application/SevenFourSeven/Raffle/GetEntriesResponse.cs
+++ src/Core/Application/SevenFourSeven/Raffle/GetEntriesResponse.cs
@@ -7,8 +7,8 @@ public class GetEntriesResponse
     public int ErorrCode { get; set; }
     public string Message { get; set; } = default!;
     public int CurrentPage { get; set; }
-    public List<EntryDto> AgentEntries { get; set; } = default!;
-    public List<EntryDto> PlayerEntries { get; set; } = default!;
+    public List<EntryDto>? AgentEntries { get; set; }
+    public List<EntryDto>? PlayerEntries { get; set; }
     public int TotalPagesWithAgentEntries { get; set; }
     public int TotalPagesWithPlayerEntries { get; set; }
 }
